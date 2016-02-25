1c10642639b67d0e771e5fb850bb8218fd675604
ScalaBcn <Scala-Developers-Barcelona-list@meetup.com>
2016-02-25 19:41:13 +0100

    Return simetric values

--- src/main/scala/AlphabetCipher.scala
+++ src/main/scala/AlphabetCipher.scala
@@ -1,4 +1,8 @@
 object AlphabetCipher {
-  def encode(row: Char, column: Char): Char = row
+  def encode(row: Char, column: Char): Char = {
+    val rowDesp = row - 'a'
+    val colDesp = column - 'a'
+    ('a' + rowDesp + colDesp).toChar
+  }
 
 }
--- src/test/scala/AlphabetCipherSpec.scala
+++ src/test/scala/AlphabetCipherSpec.scala
@@ -6,6 +6,9 @@ class AlphabetCipherSpec extends FlatSpec with Matchers {
   it should "should return same character combined with A" in {
     assert(AlphabetCipher.encode('a','a') === 'a')
     assert(AlphabetCipher.encode('c','a') === 'c')
+    assert(AlphabetCipher.encode('a','c') === 'c')
   }
+
+
 
 }
