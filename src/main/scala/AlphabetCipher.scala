ce846cbfe49be8b1bfa2ccd879890e2ac17dc0e8
Fernando Mora <moberstr@gmail.com>
2016-02-26 23:41:22 +0100

    Removed more private test methods and key generation function

--- src/main/scala/AlphabetCipher.scala
+++ src/main/scala/AlphabetCipher.scala
@@ -1,23 +1,14 @@
-import scala.collection.immutable.NumericRange.Inclusive
-
 object AlphabetCipher {
-  private val alphabet: Inclusive[Char] = 'a' to 'z'
+  private val alphabet: IndexedSeq[Char] = 'a' to 'z'
 
-  private def transformMessage(transformCharWithKey: (Char, Char) => Char)(pass: String, message: String): String =
-    key(pass).zip(message).map(transformCharWithKey.tupled).mkString
+  def cipher(pass: String, decodedMessage: String): String = vigenèreCipher(vigenèreTableTranslation(_ + _))(pass, decodedMessage)
 
-  def key(pass: String): Stream[Char] = Stream.continually(pass.toStream).flatten
+  def decipher(pass: String, codedMessage: String): String = vigenèreCipher(vigenèreTableTranslation(_ - _))(pass, codedMessage)
 
-  def cipher(pass: String, decodedMessage: String): String = transformMessage(vigenèreCipher(_ + _))(pass, decodedMessage)
+  private def vigenèreCipher(tableTranslation: (Char, Char) => Char)(pass: String, message: String): String =
+    Stream.continually(pass).flatten.zip(message).map(tableTranslation.tupled).mkString
 
-  def decipher(pass: String, codedMessage: String): String = transformMessage(vigenèreCipher(_ - _))(pass, codedMessage)
-
-  private def vigenèreCipher(op: (Int, Int) => Int)(key: Char, char: Char): Char = {
-    alphabet(
-      Math.floorMod(
-        op(alphabet.indexOf(char), alphabet.indexOf(key)),
-        alphabet.size)
-    )
-  }
+  private def vigenèreTableTranslation(op: (Int, Int) => Int)(key: Char, char: Char): Char =
+    alphabet(Math.floorMod(op(alphabet.indexOf(char), alphabet.indexOf(key)), alphabet.size))
 }
 
--- src/test/scala/AlphabetCipherSpec.scala
+++ src/test/scala/AlphabetCipherSpec.scala
@@ -3,11 +3,6 @@ import org.scalatest.{Matchers, FlatSpec}
 class AlphabetCipherSpec extends FlatSpec with Matchers {
   behavior of "AlphabetCipherSpec"
 
-  it should "return the password with the rigth length" in {
-    assert(AlphabetCipher.key("scones").take(3).mkString === "sco")
-    assert(AlphabetCipher.key("scones").take(15).mkString === "sconessconessco")
-  }
-
   it should "encode the message" in {
     assert(AlphabetCipher.cipher("scones", "meetmebythetree") === "egsgqwtahuiljgs")
     assert(AlphabetCipher.cipher("abcd", "cryptoisshortforcryptography") === "csastpkvsiqutgqucsastpiuaqjb")
