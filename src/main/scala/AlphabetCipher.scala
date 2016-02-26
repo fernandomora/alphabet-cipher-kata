9378f889903b0a9d31ccdaca905135e52948df76
Fernando Mora <moberstr@gmail.com>
2016-02-26 22:58:36 +0100

    More refactor and private methods tests removed

--- src/main/scala/AlphabetCipher.scala
+++ src/main/scala/AlphabetCipher.scala
@@ -1,25 +1,19 @@
 object AlphabetCipher {
   private val alphabet = 'a' to 'z'
-  private val alphabetSize = alphabet.size
 
-  private def transformMessage(transformCharWithKey: (Char,Char) => Char)
-                              (pass: String, message: String): String = {
-    key(pass).zip(message).map{
-      case (key, char) => transformCharWithKey(key, char)
-    }.mkString
-  }
-  
-  def cipher(pass: String, decodedMessage: String): String = transformMessage(encodeCharWithKey)(pass, decodedMessage)
-  def decipher(pass: String, codedMessage: String): String = transformMessage(decodeCharWithKey)(pass, codedMessage)
+  private def transformMessage(transformCharWithKey: (Char, Char) => Char)(pass: String, message: String): String =
+    key(pass).zip(message).map(transformCharWithKey.tupled).mkString
 
   def key(pass: String): Stream[Char] = Stream.continually(pass.toStream).flatten
-  private def alphabetCharToInt(c: Char): Int = c - alphabet(0)
-  private def intToAlphabetChar(n: Int): Char = (n + alphabet(0)).toChar
-  private val encodeOp: (Int,Int) => Int = _ + _
-  private val decodeOp: (Int,Int) => Int = _ - _
-  private def transformCharWithKey(key: Char, char: Char)(op: (Int,Int) => Int): Char = intToAlphabetChar(Math.floorMod(op(alphabetCharToInt(char), alphabetCharToInt(key)),alphabetSize))
-  def decodeCharWithKey(key:Char, char: Char): Char = transformCharWithKey(key,char)(decodeOp)
-  def encodeCharWithKey(key:Char, char: Char): Char = transformCharWithKey(key,char)(encodeOp)
 
+  def cipher(pass: String, decodedMessage: String): String = transformMessage(vigenèreCipher(_ + _))(pass, decodedMessage)
+
+  def decipher(pass: String, codedMessage: String): String = transformMessage(vigenèreCipher(_ - _))(pass, codedMessage)
+
+  private def vigenèreCipher(op: (Int, Int) => Int)(key: Char, char: Char): Char = {
+    def alphabetCharToInt(c: Char): Int = c - alphabet(0)
+    def intToAlphabetChar(n: Int): Char = (n + alphabet(0)).toChar
+    intToAlphabetChar(Math.floorMod(op(alphabetCharToInt(char), alphabetCharToInt(key)), alphabet.size))
+  }
 }
 
--- src/test/scala/AlphabetCipherSpec.scala
+++ src/test/scala/AlphabetCipherSpec.scala
@@ -3,18 +3,6 @@ import org.scalatest.{Matchers, FlatSpec}
 class AlphabetCipherSpec extends FlatSpec with Matchers {
   behavior of "AlphabetCipherSpec"
 
-  it should "return same character combined with A" in {
-    assert(AlphabetCipher.encodeCharWithKey('a','a') === 'a')
-    assert(AlphabetCipher.encodeCharWithKey('c','a') === 'c')
-    assert(AlphabetCipher.encodeCharWithKey('a','c') === 'c')
-  }
-
-  it should "return the encoded character when combined with Z" in {
-    assert(AlphabetCipher.encodeCharWithKey('z','f') === 'e')
-    assert(AlphabetCipher.encodeCharWithKey('z','a') === 'z')
-    assert(AlphabetCipher.encodeCharWithKey('z','z') === 'y')
-  }
-
   it should "return the password with the rigth length" in {
     assert(AlphabetCipher.key("scones").take(3).mkString === "sco")
     assert(AlphabetCipher.key("scones").take(15).mkString === "sconessconessco")
@@ -22,17 +10,12 @@ class AlphabetCipherSpec extends FlatSpec with Matchers {
 
   it should "encode the message" in {
     assert(AlphabetCipher.cipher("scones", "meetmebythetree") === "egsgqwtahuiljgs")
-  }
-
-  it should "decode a given character when combined with Z" in {
-    assert(AlphabetCipher.decodeCharWithKey('z', 'e') === 'f')
-    assert(AlphabetCipher.decodeCharWithKey('a', 'c') === 'c')
-    assert(AlphabetCipher.decodeCharWithKey('z', 'y') === 'z')
-    assert(AlphabetCipher.decodeCharWithKey('s', 'e') === 'm')
+    assert(AlphabetCipher.cipher("abcd", "cryptoisshortforcryptography") === "csastpkvsiqutgqucsastpiuaqjb")
   }
 
   it should "decode the message" in {
     assert(AlphabetCipher.decipher("scones", "egsgqwtahuiljgs") === "meetmebythetree")
+    assert(AlphabetCipher.decipher("abcd", "csastpkvsiqutgqucsastpiuaqjb") === "cryptoisshortforcryptography")
   }
 
 }
